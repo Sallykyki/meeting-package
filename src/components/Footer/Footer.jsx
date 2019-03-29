f777aa75f2640edf6641ecf812e757165e15fff5
Huong Pham <huong.pq12@gmail.com>
2019-03-29 23:08:51 +0200

    clean code footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,6 +6,24 @@ import SolutionsIcon from "../../assets/img/solution.png";
 import ProductsIcon from "../../assets/img/product.png";
 import MoreIcon from "../../assets/img/more.png";
 
+const solutionsLinks = ["Hotel Chains", "Independent Hotels", "Partners", "Unique Venues and Event spaces"];
+const productsLinks = [
+  "Online Booking Engine",
+  "Global Distribution System",
+  "Sales & Catering",
+  "Integrations",
+  "Analytics"
+];
+const moreLinks = ["Company", "Contact"];
+
+const relatedLinks = [
+  {title: "Solutions", icon: SolutionsIcon, links: solutionsLinks},
+  {title: "Products", icon: ProductsIcon, links: productsLinks},
+  {title: "More", icon: MoreIcon, links: moreLinks}
+];
+
+const someIconStyle = {marginRight: 0.5 + "em"};
+
 class Footer extends Component {
   render() {
     return (
@@ -16,9 +34,9 @@ class Footer extends Component {
               <strong>Follow us on social media</strong>
               <br />
               <div className="some-sections">
-                <FontAwesomeIcon icon={faFacebook} size="2x" style={{marginRight: 0.5 + "em"}} />
-                <FontAwesomeIcon icon={faTwitterSquare} size="2x" style={{marginRight: 0.5 + "em"}} />
-                <FontAwesomeIcon icon={faLinkedin} size="2x" style={{marginRight: 0.5 + "em"}} />
+                <FontAwesomeIcon icon={faFacebook} size="2x" style={someIconStyle} />
+                <FontAwesomeIcon icon={faTwitterSquare} size="2x" style={someIconStyle} />
+                <FontAwesomeIcon icon={faLinkedin} size="2x" style={someIconStyle} />
               </div>
             </div>
             <div className="col-sm-4">
@@ -27,36 +45,20 @@ class Footer extends Component {
             </div>
           </div>
           <hr className="footer-line" />
-          <div className="row">
-            <div className="col-sm-4 text-center">
-              <img src={SolutionsIcon} alt="solutions" />
-              <h4>Solutions</h4>
-              <ul>
-                <li>Hotel Chains</li>
-                <li>Independent Hotels</li>
-                <li>Partners</li>
-                <li>Unique Venues and Event spaces</li>
-              </ul>
-            </div>
-            <div className="col-sm-4 text-center">
-              <img src={ProductsIcon} alt="products" />
-              <h4>Products</h4>
-              <ul>
-                <li>Online Booking Engine</li>
-                <li>Global Distribution System</li>
-                <li>Sales &amp; Catering</li>
-                <li>Integrations</li>
-                <p>Analytics</p>
-              </ul>
-            </div>
-            <div className="col-sm-4 text-center">
-              <img src={MoreIcon} alt="more" />
-              <h4>More</h4>
-              <ul>
-                <li>Company</li>
-                <li>Contact</li>
-              </ul>
-            </div>
+          <div className="row related-links">
+            {relatedLinks.map((item, index) => (
+              <div className="col-sm-4 text-center" key={index}>
+                <img src={item.icon} alt={item.title} />
+                <h4>{item.title}</h4>
+                <ul>
+                  {item.links.map((linkItem, linkIndex) => (
+                    <li key={linkIndex}>
+                      <a href="#">{linkItem}</a>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            ))}
           </div>
         </div>
       </footer>
