88138e5ab143320c7b81efdcca37e2a35589d896
Huong Pham <huong.pq12@gmail.com>
2019-03-29 12:45:09 +0200

    modify booking detials styles

--- src/components/Body/Booking/Booking.jsx
+++ src/components/Body/Booking/Booking.jsx
@@ -1,40 +1,66 @@
 import React, {Component} from "react";
 import VenueImg from "../../../assets/img/meeting-room.jpg";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faCalendarAlt, faClock, faHotel, faUserFriends, faTag} from "@fortawesome/free-solid-svg-icons";
+import "./Booking.scss";
 
 class Booking extends Component {
   render() {
     return (
       <div className="card">
         <div className="card-body">
-          <img className="card-img-top" src={VenueImg} alt="venue img" />
-          <div className="card-img-overlay">
-            <h5 className="card-tile">Luna House</h5>
+          <h5 className="card-tile">Venue Luna House</h5>
+          <div className="img-wrapper">
+            <img className="card-img-top venue-img" src={VenueImg} alt="venue img" />
           </div>
-          <table className="table">
+
+          <table className="booking-info">
             <tbody>
               <tr>
-                <td>Date</td>
+                <td>
+                  <FontAwesomeIcon icon={faCalendarAlt} style={{marginRight: 0.5 + "em"}} color="#1C63AD" />
+                  Date
+                </td>
                 <td>Friday, February 19, 2019</td>
               </tr>
               <tr>
-                <td>Duration</td>
+                <td>
+                  {" "}
+                  <FontAwesomeIcon icon={faClock} style={{marginRight: 0.5 + "em"}} color="#1C63AD" />
+                  Duration
+                </td>
                 <td>9:00 - 17:00</td>
               </tr>
               <tr>
-                <td>Room</td>
+                <td>
+                  <FontAwesomeIcon icon={faHotel} style={{marginRight: 0.5 + "em"}} color="#1C63AD" />
+                  Room
+                </td>
                 <td>Lounge</td>
               </tr>
               <tr>
-                <td>Delegates</td>
+                <td>
+                  <FontAwesomeIcon icon={faUserFriends} style={{marginRight: 0.5 + "em"}} color="#1C63AD" />
+                  Delegates
+                </td>
                 <td>10</td>
               </tr>
               <tr>
-                <td>Price per delegate</td>
+                <td>
+                  <FontAwesomeIcon icon={faTag} style={{marginRight: 0.5 + "em"}} color="#1C63AD" />
+                  Price per delegate
+                </td>
                 <td>55 EUR</td>
               </tr>
-              <hr />
               <tr>
-                <td>TOTAL PRICE</td>
+                <td colSpan="2">
+                  <hr />
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <strong>TOTAL PRICE</strong>
+                </td>
                 <td>550 EUR</td>
               </tr>
             </tbody>
