26f3d7328ada7920e1c4ed420723b81423dee9ec
Huong Pham <huong.pq12@gmail.com>
2019-03-29 23:38:01 +0200

    clean code form

--- src/components/Body/Form/Form.jsx
+++ src/components/Body/Form/Form.jsx
@@ -1,12 +1,20 @@
 import React, {Component} from "react";
 import "./Form.scss";
 
+const form = [
+  {title: "Company Name", name: "companyName", type: "text", placeholder: "e.g. Company X"},
+  {title: "First Name", name: "firstName", type: "text", placeholder: "e.g. John"},
+  {title: "Last Name", name: "lastName", type: "text", placeholder: "e.g. Smith"},
+  {title: "Email", name: "email", type: "email", placeholder: "e.g. john@abc.xyz"},
+  {title: "Phone", name: "phone", type: "text", placeholder: "e.g. +358445673456"}
+];
 class Form extends Component {
   constructor() {
     super();
     this.state = {isChecked: false};
     this.handleChecked = this.handleChecked.bind(this);
   }
+
   handleChecked() {
     this.setState({isChecked: !this.state.isChecked});
   }
@@ -17,41 +25,22 @@ class Form extends Component {
         <div className="card-body">
           <h4 className="card-title">Your Information</h4>
           <form>
-            <div className="form-group">
-              <label>Company Name</label>
-              <br />
-              <input className="form-control personal-info" name="companyname" placeholder="e.g. Company X" required />
-            </div>
-            <div className="form-group">
-              <label>First Name</label>
-              <br />
-              <input className="form-control personal-info" name="firstname" placeholder="e.g. John" required />
-            </div>
-            <div className="form-group">
-              <label>Last Name</label>
-              <br />
-              <input className="form-control personal-info" name="lastname" placeholder="e.g. Smith" required />
-            </div>
-            <div className="form-group">
-              <label>Email</label>
-              <br />
-              <input
-                type="email"
-                className="form-control personal-info"
-                name="email"
-                placeholder="e.g. john@xyz.abc"
-                required
-              />
-            </div>
-            <div className="form-group">
-              <label>Phone</label>
-              <br />
-              <input className="form-control personal-info" name="phone" placeholder="e.g. +358445673456" required />
-            </div>
+            {form.map((item, index) => (
+              <div key={index} className="form-group">
+                <label htmlFor={item.name}>{item.title}</label>
+                <br />
+                <input
+                  className="form-control personal-info"
+                  name={item.name}
+                  placeholder={item.placeholder}
+                  id={item.name}
+                  required
+                />
+              </div>
+            ))}
             <div className="form-check">
-              <input className="form-check-input" type="checkbox" onChange={this.handleChecked} />
-              <label className="form-check-label" />
-              Agree to terms
+              <input className="form-check-input" name="term" id="term" type="checkbox" onChange={this.handleChecked} />
+              <label htmlFor="term">Agree to terms</label>
             </div>
             <button type="submit" className="btn btn-primary" disabled={!this.state.isChecked}>
               <strong>Continue</strong>
