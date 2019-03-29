5b9b6d10b0fa32e76ef8ce6affd6bf53984006f9
Huong Pham <huong.pq12@gmail.com>
2019-03-29 13:45:52 +0200

    add submit btn, modify form styles

--- src/components/Body/Form/Form.jsx
+++ src/components/Body/Form/Form.jsx
@@ -2,35 +2,58 @@ import React, {Component} from "react";
 import "./Form.scss";
 
 class Form extends Component {
+  constructor() {
+    super();
+    this.state = {isChecked: false};
+    this.handleChecked = this.handleChecked.bind(this);
+  }
+  handleChecked() {
+    this.setState({isChecked: !this.state.isChecked});
+  }
+
   render() {
     return (
       <div>
         <form>
           <div className="form-group">
-            <div className="form-group">
-              <label>Company Name</label>
-              <br />
-              <input className="form-control" name="companyname" placeholder="e.g. Company X" />
-            </div>
+            <label>Company Name</label>
+            <br />
+            <input className="form-control personal-info" name="companyname" placeholder="e.g. Company X" required />
+          </div>
+          <div className="form-group">
             <label>First Name</label>
             <br />
-            <input className="form-control" name="firstname" placeholder="e.g. John" />
+            <input className="form-control personal-info" name="firstname" placeholder="e.g. John" required />
           </div>
           <div className="form-group">
             <label>Last Name</label>
             <br />
-            <input className="form-control" name="lastname" placeholder="e.g. Smith" />
+            <input className="form-control personal-info" name="lastname" placeholder="e.g. Smith" required />
           </div>
           <div className="form-group">
             <label>Email</label>
             <br />
-            <input type="email" className="form-control" name="email" placeholder="e.g. john@xyz.abc" />
+            <input
+              type="email"
+              className="form-control personal-info"
+              name="email"
+              placeholder="e.g. john@xyz.abc"
+              required
+            />
           </div>
           <div className="form-group">
             <label>Phone</label>
             <br />
-            <input className="form-control" name="phone" placeholder="e.g. +358445673456" />
+            <input className="form-control personal-info" name="phone" placeholder="e.g. +358445673456" required />
           </div>
+          <div className="form-check">
+            <input class="form-check-input" type="checkbox" onChange={this.handleChecked} />
+            <label class="form-check-label" for="defaultCheck1" />
+            Agree to terms
+          </div>
+          <button type="submit" class="btn btn-primary" disabled={!this.state.isChecked}>
+            <strong>Continue</strong>
+          </button>
         </form>
       </div>
     );
